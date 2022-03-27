c40ff857bf8020464b60ff3ff2766293553af40f
Louis <louiscflin@gmail.com>
2022-03-27 16:49:00 +1300

    feat: footer

---
+++ components/Footer.tsx
@@ -0,0 +1,32 @@
+import classes from "./Footer.module.scss";
+
+const TABS = [
+  {
+    label: "email",
+    href: "mailto:louiscflin@gmail.com",
+  },
+  {
+    label: "linkedIn",
+    href: "https://www.linkedin.com/in/louiscflin/",
+  },
+  {
+    label: "gitHub",
+    href: "https://github.com/louis-cf-lin",
+  },
+];
+
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.inner}>
+        {TABS.map((t) => (
+          <a key={t.href} href={t.href} target="_blank" rel="noreferrer">
+            {t.label}
+          </a>
+        ))}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
