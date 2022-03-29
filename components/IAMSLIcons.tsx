2e6b9f8d39ede5c90ab7b959fdbd7148b5e26486
Louis <louiscflin@gmail.com>
2022-03-29 19:53:18 +1300

    feat: iamsl mobile

--- components/IAMSLIcons.tsx
+++ components/IAMSLIcons.tsx
@@ -1,3 +1,4 @@
+import { Parallax } from "react-scroll-parallax";
 import classes from "./IAMSLIcons.module.scss";
 
 const COLORS = [
@@ -7,41 +8,40 @@ const COLORS = [
 ];
 
 interface Props {
+  className: string;
   flexDirection?: "row" | "column";
-  size?: number;
 }
 
 const IAMSLIcons = ({
+  className,
   flexDirection = "row",
-  size = 96,
 }: Props): JSX.Element => {
   return (
     <div className={classes.container} style={{ flexDirection: flexDirection }}>
-      {COLORS.map((c) => (
-        <div
-          key={c.bg}
-          className={classes.wrapper}
-          style={{
-            background: c.bg,
-            height: size,
-            width: size,
-            color: c.shadow,
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="40%"
-            height="40%"
-            viewBox="0 0 50 50"
-            fill="none"
+      {COLORS.map((c, i) => (
+        <Parallax key={c.bg} translateX={[i * 10 - 10 + "px", "0px"]}>
+          <div
+            className={`${classes.wrapper} ${className}`}
+            style={{
+              background: c.bg,
+              color: c.shadow,
+            }}
           >
-            <path
-              d="M5 0V20C5 33.8071 16.1929 45 30 45H50M40 25H30C27.2386 25 25 22.7614 25 20V5H50"
-              stroke="#F58C31"
-              strokeWidth="10"
-            />
-          </svg>
-        </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="40%"
+              height="40%"
+              viewBox="0 0 50 50"
+              fill="none"
+            >
+              <path
+                d="M5 0V20C5 33.8071 16.1929 45 30 45H50M40 25H30C27.2386 25 25 22.7614 25 20V5H50"
+                stroke="#F58C31"
+                strokeWidth="10"
+              />
+            </svg>
+          </div>
+        </Parallax>
       ))}
     </div>
   );
--- pages/projects/iamsl-app.tsx
+++ pages/projects/iamsl-app.tsx
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
+import { Parallax } from "react-scroll-parallax";
 import Header from "../../components/Header";
 import IAMSLIcons from "../../components/IAMSLIcons";
 import Page from "../../components/Page";
+import ProjectsNav from "../../components/ProjectsNav";
 
 import classes from "../../styles/iamsl.module.scss";
 
@@ -14,13 +16,49 @@ const IAMSL = () => {
         <meta name="description" content="Hi, I'm Louis and I write code." />
         <link rel="icon" href="/icon.ico" />
       </Head>
+      <Header />
       <main className={classes.main}>
-        <Header />
-        {/* <div className={classes.ipad}>
-          <Image src="/ipad.png" layout="fill" alt="App render" />
+        <ProjectsNav active="iamsl-app" />
+        <div className={`${classes.img} ${classes.ipad}`}>
+          <Image src="/iamsl-ipad.png" layout="fill" alt="IAMSL app render" />
         </div>
-        <h2 className={classes.h2}>Commercially-contracted data capture app</h2> */}
-        <IAMSLIcons />
+        <Parallax translateY={["20px", "-100px"]}>
+          <h2 className={`${classes.h2} ${classes.t1}`}>
+            Commercial data capture app
+          </h2>
+        </Parallax>
+        <IAMSLIcons className={`${classes.img} ${classes.icon}`} />
+        <Parallax translateY={["-100px", "20px"]}>
+          <h2 className={`${classes.h2} ${classes.t2}`}>
+            Built with React Native
+          </h2>
+        </Parallax>
+        <Parallax
+          translateX={["0px", "-20px"]}
+          translateY={["10px", "-20px"]}
+          style={{ zIndex: 1 }}
+        >
+          <div className={`${classes.img} ${classes.corner}`}>
+            <Image src="/iamsl-corner.png" layout="fill" alt="IAMSL app PDF" />
+          </div>
+        </Parallax>
+        <Parallax translateY={["-20px", "20px"]}>
+          <h2 className={`${classes.h2} ${classes.t3}`}>
+            Generate PDF&apos;s with dynamic values
+          </h2>
+        </Parallax>
+        <Parallax scale={[1.25, 0.8]}>
+          <div className={`${classes.img} ${classes.collage}`}>
+            <Image
+              src="/iamsl-collage.png"
+              layout="fill"
+              alt="IAMSL app showcase"
+            />
+          </div>
+        </Parallax>
+        <h2 className={`${classes.h2} ${classes.t4}`}>
+          Includes <b>native filesystem</b> read-write
+        </h2>
       </main>
     </Page>
   );
