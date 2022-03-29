73d8152b98e55caee3c48d4651599a6a44759619
Louis <louiscflin@gmail.com>
2022-03-29 22:32:37 +1300

    wip

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix:
     process.env.NODE_ENV === "production"
-      ? process.env.NEXT_PUBLIC_BASE_PATH
+      ? process.env.NEXT_PUBLIC_BASE_PATH + "/"
       : "",
 };
 
