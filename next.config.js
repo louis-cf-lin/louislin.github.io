3392ec136e08824311bf740f77dc3bab44d964ac
Louis <louiscflin@gmail.com>
2022-03-29 21:27:01 +1300

    fix: prefix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
-  assetPrefix: "/out/",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 };
 
 module.exports = nextConfig;
