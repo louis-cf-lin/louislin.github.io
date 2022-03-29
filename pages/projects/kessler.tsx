8cf2b85703ada86a7ceb93a426c6f66aa2f172cf
Louis <louiscflin@gmail.com>
2022-03-30 02:17:40 +1300

    feat: kessler

--- pages/projects/kessler.tsx
+++ pages/projects/kessler.tsx
@@ -1,11 +1,25 @@
 import Head from "next/head";
+import { ChangeEvent, FormEventHandler, useRef, useState } from "react";
 import Header from "../../components/Header";
+import { EnterI, LockI } from "../../components/Icon";
 import Page from "../../components/Page";
 import ProjectsNav from "../../components/ProjectsNav";
 
 import classes from "../../styles/kessler.module.scss";
 
 const Kessler = () => {
+  const ref = useRef<HTMLInputElement>(null);
+  const [error, setError] = useState("");
+
+  const submitHandler: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (!ref.current?.value) {
+      setError("Password missing");
+    } else {
+      setError("Incorrect password");
+    }
+  };
+
   return (
     <Page>
       <Head>
@@ -16,6 +30,29 @@ const Kessler = () => {
       <Header />
       <main className={classes.main}>
         <ProjectsNav active="kessler" />
+        <div className={classes.container}>
+          <form className={classes.form} onSubmit={submitHandler}>
+            <label>Classified space stuff.</label>
+            <div
+              className={`${classes.inputWrapper} ${
+                error ? classes.error : ""
+              }`}
+            >
+              <input type="password" ref={ref} onChange={() => setError("")} />
+              <LockI height={26} width={26} className={classes.lock} />
+              <EnterI height={26} width={26} className={classes.enter} />
+            </div>
+            <div className={classes.errorMessage}>{error}</div>
+            <p>
+              But I&apos;m down for a chat&mdash;
+              <a href="mailto:louiscflin@gmail.com">get in touch</a>.
+            </p>
+            <span>
+              Kessler is a student-run project from the Auckland Programme for
+              Space Systems.
+            </span>
+          </form>
+        </div>
       </main>
     </Page>
   );
