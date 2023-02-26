5754c6a9593262690822f2676ca7385d1c988ba1
Louis Lin <louis@vxt.co.nz>
2023-02-27 01:24:48 +1100

    Added no jekyll

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /** Tell Next.js where the `public` folder is */
-  assetPrefix: isProd ? "/louis-cf-lin.github.io/" : "",
   reactStrictMode: true,
   /** @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable */
   images: {
