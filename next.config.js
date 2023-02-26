121220213cdc2a0fabf9df6d1f0194269c68befb
Louis Lin <louis@vxt.co.nz>
2023-02-27 00:37:58 +1100

    Revert to working fix

--- next.config.js
+++ next.config.js
@@ -2,14 +2,12 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /** Tell Next.js where the `public` folder is */
-  assetPrefix: isProd ? "/louis-cf-lin.github.io/" : "/",
   reactStrictMode: true,
   /** @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable */
   images: {
     unoptimized: true,
-    // loader: "akamai",
-    // path: "/",
+    loader: "akamai",
+    path: "/",
   },
 };
 
