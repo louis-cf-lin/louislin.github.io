36f8c8952bfc41a3b056c951c5279f2819a940f5
Louis Lin <louis@vxt.co.nz>
2023-02-27 01:35:51 +1100

    Fix

--- next.config.js
+++ next.config.js
@@ -2,14 +2,12 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: isProd ? "/" : "",
   reactStrictMode: true,
   /** @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable */
   images: {
     unoptimized: true,
-    loader: "akamai",
-    path: "/",
   },
-  basePath: "",
 };
 
 module.exports = nextConfig;
